6cc5cdc479619eba55694fb1d27cde5d24d9a050
MarlonRoches <roches856@gmail.com>
2021-08-06 22:05:39 -0600

    Implementando EJS

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app =express()
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose');
 
+//variable de entorno
 
 
 const user = "admin_ED2"
@@ -10,6 +11,11 @@ const password = "ytmlDR0qqKiHhGVE"
 const dbName = "proyectoED2"
 
 const uri = `mongodb+srv://${user}:${password}@cluster0.c0tze.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+//motor de plantillas
+app.set("view engine", "ejs")
+//motor de plantillas
+app.set("views", __dirname+ "/views")
 
 mongoose.connect(uri, 
     { 
@@ -20,12 +26,26 @@ mongoose.connect(uri,
   //error 
   .catch(e => console.log('error de conexión', e))
 
+
+
 app.get('/',(req,res)=>{
-    res.send('to piola')
+    res.render('index', {
+        titulo: "Hola pai"
+    })
 })
 
 app.use('/manejo',require('./routes/Manejo'))
 app.use('/productos',require('./routes/Productos'))
 app.use('/sucursales',require('./routes/Sucursales'))
+
+
+app.use((req,res) => {
+    // const url = req.originalUrl
+    res.status(404).render('404', {
+        titulo: req.originalUrl,
+        range : Date.now()
+    })
+    console.log(req.originalUrl)
+})
 
 app.listen(port, () => console.log('ED2  escuchando en el puerto 3000'))
